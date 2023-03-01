6d4cb7a08d26cf8f1af414defbedc74f9bd9829f
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-03-01 10:34:41 -0300

    style.css

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const emailInput = document.querySelector("#email");
+const btnS = document.querySelector("#btn-submit");
+const errorMessage = document.querySelector("#res");
+const checkEmailform = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const timeOut = setInterval(() => {
+  errorMessage.style.display = "none";
+}, 4000);
+
+btnS.addEventListener("click", function () {
+  if (emailInput.value.length === 0) {
+    errorMessage.style.display = "block";
+    errorMessage.style.color = "#ff5263";
+    errorMessage.textContent =
+      "Whoops! It looks like you forgot to add your email";
+    timeOut;
+  } else if (
+    !emailInput.value.match(checkEmailform) &&
+    emailInput.value.length !== 0
+  ) {
+    errorMessage.style.display = "block";
+    errorMessage.style.color = "#ff5263";
+    errorMessage.textContent = "Please provide a valid email address";
+    timeOut;
+  } else {
+    errorMessage.style.display = "block";
+    errorMessage.style.color = "green";
+    errorMessage.textContent = "We sent you a gift, check it out!";
+    timeOut;
+  }
+});
